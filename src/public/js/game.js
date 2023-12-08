e1741c3e050e07fcc621789b540290f00be0457d
yony-bentancor <yonybentancor@gmail.com>
2023-12-08 11:20:21 -0300

    css

--- src/public/js/game.js
+++ src/public/js/game.js
@@ -4,127 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
   let playerY = 0;
   let isJumping = false;
   let jumpDirection = 1; // 1 para derecha, -1 para izquierda
-  let lastPressedKey = ""; // Variable para rastrear la última tecla presionada
-
-  document.addEventListener("keydown", (event) => {
-    if (event.repeat) return; // Evitar repetición de teclas manteniendo presionada
-
-    switch (event.key) {
-      case "ArrowLeft":
-        playerX -= 10;
-        jumpDirection = -1;
-        lastPressedKey = "ArrowLeft";
-        break;
-      case "ArrowRight":
-        playerX += 10;
-        jumpDirection = 1;
-        lastPressedKey = "ArrowRight";
-        break;
-      case " ":
-        if (!isJumping) {
-          jump();
-        }
-        break;
-    }
-
-    updatePlayerPosition();
-  });
-
-  document.addEventListener("keyup", () => {
-    // Cambiar el gif solo si no se ha presionado ninguna tecla después de soltar la última tecla
-    setTimeout(() => {
-      if (lastPressedKey === "ArrowLeft") {
-        changePlayerGif("centerAlternativa");
-      } else if (lastPressedKey === "ArrowRight") {
-        changePlayerGif("center");
-      }
-    }, 100);
-
-    lastPressedKey = ""; // Restablecer la última tecla presionada
-  });
-
-  function updatePlayerPosition() {
-    player.style.left = playerX + "px";
-    player.style.top = playerY + "px";
-  }
-
-  function jump() {
-    isJumping = true;
-    let jumpHeight = 100;
-    let jumpSpeed = 5;
-    let gravity = 7.5;
-    let initialY = playerY;
-    let jumpDistance = 50; // Distancia horizontal del salto
-
-    function jumpStep() {
-      if (jumpHeight > 0) {
-        playerY -= jumpSpeed;
-        playerX += jumpSpeed * jumpDirection; // Mover en la dirección actual
-        jumpHeight -= jumpSpeed;
-        updatePlayerPosition();
-        requestAnimationFrame(jumpStep);
-      } else {
-        fall();
-      }
-    }
-
-    function fall() {
-      if (playerY < initialY) {
-        playerY += gravity;
-        playerX += gravity * jumpDirection; // Mover en la dirección actual
-        updatePlayerPosition();
-        requestAnimationFrame(fall);
-      } else {
-        playerY = initialY;
-        isJumping = false;
-        updatePlayerPosition();
-      }
-    }
-
-    jumpStep();
-  }
-
-  function changePlayerGif(direction) {
-    let gifPath;
-
-    switch (direction) {
-      case "left":
-        gifPath =
-          "url('https://dinamitadog-01a0d2a58fb2.herokuapp.com/static/img/imgGallinaLeft.gif')";
-        break;
-      case "right":
-        gifPath =
-          "url('https://dinamitadog-01a0d2a58fb2.herokuapp.com/static/img/imgGallinaRight.gif')";
-        break;
-      case "center":
-        gifPath =
-          "url('https://dinamitadog-01a0d2a58fb2.herokuapp.com/static/img/imgGallinaCenter.gif')";
-        break;
-      case "centerAlternativa":
-        gifPath =
-          "url('https://dinamitadog-01a0d2a58fb2.herokuapp.com/static/img/imgGallinaCenterAlternativa.gif')";
-        break;
-      default:
-        break;
-    }
-
-    player.style.backgroundImage = gifPath;
-
-    // Agregar o quitar la clase 'left' según la dirección
-    if (direction === "left") {
-      player.classList.add("left");
-    } else {
-      player.classList.remove("left");
-    }
-  }
-});
-
-/* document.addEventListener("DOMContentLoaded", () => {
-  const player = document.getElementById("player");
-  let playerX = 0;
-  let playerY = 0;
-  let isJumping = false;
-  let jumpDirection = 1; // 1 para derecha, -1 para izquierda
 
   document.addEventListener("keydown", (event) => {
     switch (event.key) {
@@ -222,7 +101,7 @@ document.addEventListener("DOMContentLoaded", () => {
       player.classList.remove("left");
     }
   }
-}); */
+});
 
 /* document.addEventListener("DOMContentLoaded", () => {
   const player = document.getElementById("player");
